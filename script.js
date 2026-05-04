2e056162a8fa9a38a59fb7af39c1b42b10989d9d
viccodev <osoriovictor.dev@gmail.com>
2026-05-04 07:13:58 -0400

    Deleted comms, added two messages to give feed back

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-/* 
-Hay que hacer un lienzo de 16x16 o lo que el 
-usuario elija, (maximo 100x100). Cuando el 
-mouse pase por encima de cada cuadrito que el
-cuadro obtenga +10% de opacidad + cambie o 
-mantenga el mismo color (prefiero el mismo, pero
-si el proyecto pide otra cosa esta bien).
-
-Ahora, quiero varias cosas: 
-- Boton Cuadricula: Tamano del lienzo "n*n"
-- Boton Reset, CanvasSize, y tipo de dibujo.
-- Propiedades iniciales y de cambio.
-- Control de cada cuadro pa formar 100x100
-bien hecho ( no se como hacer eso aun xd)
-- 
-
-El Grid esta listo, faltan los events listeners:
-Uno para cambiar el grid y el tamano (listo)
-Uno para cada boton:
-- Reset (listo)
-- Color (random por cada intento o elegido)
-- Borde (quitar y poner el borde al grid)
-- MouseOver o click para dibujo
-En parte todos estos son propiedades del grid principal.
-Por lo cual, que cada uno tenga la mania de agregar y quitar event listener no seria una locura pero seria muy pesado.
-*/
 
 // General body options to avoid problems - DONE
 document.body.style.backgroundColor = "gray";
@@ -65,6 +39,8 @@ function createCanvasSize(alto, ancho){
     for(let i = 0; i < alto*ancho; i++) {
         if(alto > 100 || ancho > 100){
             return alert("PLEASE DONT USE MORE THAN 100!, you pc can explode!");
+        } else if (typeof alto !== "number" || typeof ancho !== "number"){
+            return alert("Please use only numbers");
         }
     let canvasSquare = document.createElement("div");
     canvasSquare.style.width = `calc(100% / ${ancho})`; 
@@ -186,23 +162,6 @@ function drawMode(e){
         btnDrawMode.value = "click";   
         btnDrawMode.textContent = `Draw Mode CLICK`;    
     }
-    /* old
-    target = e.type;
-    if(target === "click"){
-        canvasContainer.removeEventListener("click",(target) =>{
-    targ = target.target; drawing(targ, colorChoice); });
-
-    canvasContainer.addEventListener("mouseover",(target) =>{
-    targ = target.target; drawing(targ, colorChoice); });
-
-    } else if(target === "mouseover") {
-        canvasContainer.removeEventListener("mouseover",(target) =>{
-    targ = target.target; drawing(targ, colorChoice); });
-
-        canvasContainer.addEventListener("click",(target) =>{
-    targ = target.target; drawing(targ, colorChoice); });
-    btnDrawMode.value = "click";
-    } */
 };
 canvasContainer.addEventListener("click", drawingHandler);
 // Funcion para dibujar
@@ -266,173 +225,7 @@ function drawing(e, colorC){
         }
     }
 
-        //let opac = Number(e.target.style.opacity) + 0.1;
-        // let bC = target.style.backgroundColor;
-        // Console to debug: console.log(opac);
-        /*if(Number(target.style.opacity) > 0){
-            target.style.opacity = `${opac}`;
-        } else if(Number(target.style.opacity) === 0)
-            target.style.opacity = "0.1";
-            if(colorChoice){
-                target.style.backgroundColor = colorC;
-            }else if(target.style.background){
-            target.style.backgroundColor = `RGB(${randomColor().join(",")})`;
-            }
-            
-}
-            */
-};
-
-/*
-
-
-canvasContainer.addEventListener("click", (e) => {
-    let target = e.target;
-    drawing(target, colorChoice);
-});
-
-
-
-// BUTTON MOUSE MODE
-btnDrawMode = document.createElement("button");
-let mouseToggle = "click";
-btnDrawMode.textContent = `Draw Mode`;
-btnDrawMode.setAttribute("id", "drawMode");
-btnDrawMode.addEventListener("click", (e) => {
-    let canvType = document.querySelector("canvasContainer");
-    drawMode(e, mouseToggle)
-})
-btnContainer.appendChild(btnDrawMode);
-
-// Funcion para reemplazar listener click por mouseover
-
-function drawMode(e, mouseMode){
-    if(mouseMode === "click"){
-        canvasContainer.removeEventListener("click", drawing(targ, colorChoice));
-    canvasContainer.addEventListener("mouseover", drawing(targ, colorChoice));
-    mouseToggle = "mouseover"
-    } else if (mouseMode === "mouseover"){
-        canvasContainer.removeEventListener("mouseover", drawing(targ, colorChoice));
-        canvasContainer.addEventListener("click", drawing(targ, colorChoice));
-        mouseToggle = "click";
-    }
-};
-
-// Funcion para dibujar 
-function drawing(targ, colorC){
-    if(targ === canvasContainer){
-        undefined;
-    }else{
-        targ.style.backgroundColor = colorC;
-    }
-    
-};
-/*
-
-
-function drawMode(e, mouseMode){
-    target = e.target;
-
-    if(target.type === "click"){
-        canvasContainer.removeEventListener("click", drawing(e, colorChoice));
-    canvasContainer.addEventListener("mouseover", drawing(e, colorChoice));
-    } else if (mouseMode === "mouseover"){
-        canvasContainer.removeEventListener("mouseover", drawing(e, colorChoice));
-        canvasContainer.addEventListener("click", drawing(e, colorChoice));
-    }
-};
-
-
-// Draw on the selected node. - DONE
-
-
-
-
-
-
-
-
-/* Old Code for Now
-
-// BUTTON MOUSE MODE
-btnDrawMode = document.createElement("button");
-let mouseToggle = "click";
-btnDrawMode.textContent = `Draw Mode: ${mouseToggle}`;
-btnDrawMode.setAttribute("id", "drawMode");
-btnDrawMode.addEventListener("click", (e) => {
-})
-// toggle MOuse Functions
-
-// Mouse Draw Mode
-
-function drawMode(e, mouseMode){
-    if(canvasContainer.e.type === "click"){
-        canvasContainer.removeEventListener("click", drawing(e, color, opacity));
-        canvasContainer.addEventListener("mouseover", drawing(e, color, opacity));
-    } else if (mouseMode === "mouseover"){
-        canvasContainer.removeEventListener("mouseover", drawing(e, color, opacity));
-        canvasContainer.addEventListener("click", drawing(e, color, opacity));
-    }
-};
-// Draw on the selected node.
-function drawing(e, color, opacity){
-    target = e.target;
-    if(target === canvasContainer){
-        undefined;
-    }else{
-        target.style.backgroundColor(color);
-    }
-    
-};
-btnContainer.appendChild(btnDrawMode);
-
-
-
-//
-
-canvasContainer.addEventListener(mouseToggle, (e) =>{
-    trg = e.target;
-    if(trg === canvasContainer){
-        undefined;
-    }else{
-        e.target.style.backgroundColor = "red";
-    }
-})
-
-//button to change color
-
-let btnColor = document.createElement("button");
-btnColor.textContent = "Change Color";
-btnColor.addEventListener("click", () => {
-    
-})
-
-//Button to show or hide borders:
-
-let btnShowBorders = document.createElement("button");
-btnShowBorders.textContent = "Show Borders";
-btnShowBorders.addEventListener("click", (e) => {
-    let canvasSquares = document.querySelectorAll("canvasContainer > div");
-    canvasSquares.forEach((e) => {
         
-        canvasSquares.classList.toggle("outline");
-    })
-})
-btnContainer.appendChild(btnShowBorders);
-*/
+};
 
 
-
-
-
-// ALL ELEMENT STYLE:
-/* COMMENT THIS FOR USE OLD CODE LATER
-document.body.style.width = "100%";
-document.body.style.height = "100%";
-
-
-
-
-
-
-*/
